ceac90b150b246efaa526febba4a8c4521d0efeb
Akachukwu Chukwuemerie David <63329343+Imdavyking@users.noreply.github.com>
2024-08-23 13:52:28 +0100

    fixing bugs

--- utils/contract-utils.ts
+++ utils/contract-utils.ts
@@ -1,11 +1,3 @@
 export const getEvmAddress = async (account_id: string) => {
-  const env = useRuntimeConfig().public
-  const url = `${env.hederaBaseUrl}/api/v1/accounts/${account_id}?limit=1`;
-
-  try {
-    const res = await $fetch<{evm_address:string}>(url);
-    return res?.evm_address
-  } catch (error: any) {
-    return error?.message || "Something went wrong"
-  }
-}
+  return account_id;
+};
